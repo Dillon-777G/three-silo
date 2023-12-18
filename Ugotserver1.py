a6b175a44f0d5e525bfe9b6e00bf09c812015844
Dillon-777G <d777g@protonmail.com>
2023-12-18 15:47:17 -0800

    Migrated to OpenAI instead of vertex ai

--- Ugotserver1.py
+++ Ugotserver1.py
@@ -1,34 +1,30 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from google.cloud import aiplatform
-import vertexai
-from vertexai.language_models import ChatModel, InputOutputTextPair, ChatSession
+from openai import OpenAI
+import os
 
 app = Flask(__name__)
 CORS(app)
 
+# Initialize the OpenAI client
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
-
-vertexai.init(project="glutenizer420", location="us-central1")
-chat_model = ChatModel.from_pretrained("chat-bison")
-parameters = {
-    "max_output_tokens": 1000,
-    "temperature": 0.2,
-    "top_p": 0.8,
-    "top_k": 40
-}
 @app.route('/chat', methods=['POST'])
 def chat_endpoint():
     user_message = request.json['message']
-    
-    chat = chat_model.start_chat(
-        context="You are a chef that specializes in designing recipes and diet plans which are vegan and gluten free. You will provide recipes as well as advice on meal prepping. When asked for a weekly meal plan, you will provide breakfast, lunch, and dinner options for each weekday. After someone tells you their dietary restrictions you will immediately respond with a meal plan or recipe."
-    )
+    print("Received message:", user_message)
+
     try:
-        response = chat.send_message(user_message)
-        print(response.text)
-        return jsonify({"reply": response.text})
+        chat_completion = client.chat.completions.create(
+            messages=[{"role": "user", "content": user_message}],
+            model="gpt-3.5-turbo",
+        )
+
+        # Correctly accessing the response
+        reply = chat_completion.choices[0].message.content
+        return jsonify({"reply": reply})
     except Exception as e:
+        print("Error:", str(e))
         return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
