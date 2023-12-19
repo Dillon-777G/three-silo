9d5c9efab1ec09b9197323fd21dc3900702ce53e
Dillon-777G <d777g@protonmail.com>
2023-12-19 00:02:29 -0800

    css touch up

--- Ugotserver1.py
+++ Ugotserver1.py
@@ -3,12 +3,19 @@ from flask_cors import CORS
 from openai import OpenAI
 import os
 
+
+
+
 app = Flask(__name__)
 CORS(app)
 
 # Initialize the OpenAI client
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
+
+#This Python script defines a Flask web server with an endpoint /chat that handles POST requests. 
+# When a message is received, it extracts the user's message from the request, prints it for logging,
+# and then sends this message to the OpenAI GPT-3.5-turbo model using the OpenAI API.
 @app.route('/chat', methods=['POST'])
 def chat_endpoint():
     user_message = request.json['message']
